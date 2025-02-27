7644a028f1ba2f3ba9d49a757ca5040d092d617b
MdShajalalsojib <shahjalalsojib088@gmail.com>
2025-02-27 13:17:05 +0600

    coshout

---
+++ JavaScript/AddMoney.js
@@ -0,0 +1,20 @@
+document.getElementById('Add_money').addEventListener('click',function(event){
+    event.preventDefault();
+    const Amount = document.getElementById("Amount").value;
+     const convertedAmount = parseFloat(Amount);
+     const pin = document.getElementById("Enter-pin").value;
+     const convertedPin = parseInt(pin);
+
+     const mainBalance= document.getElementById("main-balance").innerText;
+     const convert = parseFloat(mainBalance);
+
+     if(convertedPin===1234){
+        // console.log("This pin is ok")
+        const sum = convert + convertedAmount;
+        document.getElementById("main-balance").innerText = sum;
+
+     }
+     else{
+        console.log("Please check your PIN number")
+     }
+})
---
+++ JavaScript/Casout.js
@@ -0,0 +1,12 @@
+document.getElementById("cashout").style.display="none";
+document.getElementById("addMoney").style.display = "none";
+
+document.getElementById("add-money-box").addEventListener("click",function(){
+      document.getElementById("cashout").style.display="none";
+      document.getElementById("addMoney").style.display = "block";
+})
+
+ document.getElementById("cas-hout-box").addEventListener("click",function(){
+    document.getElementById("cashout").style.display="block";
+    document.getElementById("addMoney").style.display = "none";
+ })
